3bcfbd047fe74836158bb74bb67bd39cd448eeca
Darren Diggs <darrendiggs00@gmail.com>
2021-04-21 20:59:44 -0500

    Basic Scaffolding

---
+++ src/components/Map.js
@@ -0,0 +1,15 @@
+
+
+import GoogleMap from 'google-map-react'
+
+const Map = () => {
+  return (
+    <div className="map">
+
+      <GoogleMap></GoogleMap>
+      
+    </div>
+  )
+}
+
+export default Map
