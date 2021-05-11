5ed1094e3679cb3d65bacd90a255b8498a0f63ca
Darren Diggs <darrendiggs00@gmail.com>
2021-05-11 18:52:16 -0500

    Nasa Api Link

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   useEffect(() => {
     const fetchEvents = async () => {
       setLoading(true)
-      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?source=InciWeb,EO') 
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
       const { events } = await res.json()
 
       setEventData(events)
